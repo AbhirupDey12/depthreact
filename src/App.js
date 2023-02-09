e9c2f7a99381b4ab0b87f55848a3aa3589d996c5
AbhirupDey12 <abhirupd470@gmail.com>
2023-02-09 11:05:10 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,60 +1,16 @@
-import React from "react";
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import Hello from "./components/Hello";
-import Message from "./components/Message";
-import Counter from "./components/Counter";
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import EventBind from "./components/EventBind";
-import ParentComponent from "./ParentComponent";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/NameList";
-import Stylesheet from "./components/Stylesheet";
-import Inline from "./components/Inline";
-import './appStyles.css' ;
-import styles from './appStyles.module.css' ;
+import React, { useState, useEffect } from "react";
 
-class App extends React.Component {
+export default function App() {
 
-  constructor() {
-    super();
-  }
+  return (
+    
+    <h1>
+      Hello world
+    </h1>
 
-  render() {
-    return (
-      <div>
-        {/* <h1 className="error" >Error</h1> */}
-        {/* <h1 className= {styles.sucess} > Success </h1> */}
-        {/* <Inline /> */}
-        {/* <Stylesheet primary = {true} /> */}
-        {/* <NameList /> */}
-        {/* <UserGreeting /> */}
-        {/* <ParentComponent /> */}
-        {/* <EventBind /> */}
-        {/* <FunctionClick /> */}
-        {/* <ClassClick /> */}
-        {/* <Greet name="Diana" heroName="Wonderwoman" /> */}
-        {/* <Counter /> */}
-        {/* <Message /> */}
-        {/* <Greet name="Bruce" heroName="Batman" >
-          <p>This is children props </p>
-        </Greet>
-        <Greet name="Clark" heroName="Superman">
-          <button>Action</button>
-        </Greet>
-        <Welcome name="Clark" heroName="Superman" />
-      <Welcome  name="Diana" heroName="Wonderwoman"   /> */}
-        {/* <Hello /> */}
-        {/* <Welcome name="Bruce" heroName="Batman" /> */}
-      </div>
-    )
-  }
-}
+  );
 
-export default App;
-
-// I love ritik sir and I can go to any level to make him proud of me...
+};
 
 
 
@@ -64,6 +20,52 @@ export default App;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let key = "726289ec156b0e5c8e53ce64db4a1b2f";
+//   const [city, setCity] = useState("");
+//   const [weather, setWeather] = useState(null);
+
+//   const getWeather = () =>
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&apiid=${key}`)
+//       .then((response) => response.json())
+//       .then((data) => console.log(data)) //setWeather(data))
+//       .catch((error) => console.error(error))
+
+
+//   return (
+//     <div>
+//       <h1>Weather in {city}:</h1>
+//       {weather !== null && <p> Temperature : {weather.main.temp} °C </p>}
+//       {weather !== null && <p> Conditions : {weather.weather[0].description}  </p>}
+//       <input
+//         type="text"
+//         value={city}
+//         onChange={(e) => setCity(e.target.value)}
+//       />
+//       <button onClick={getWeather} >Get Weather</button>
+//     </div>
+//   )
 
 
 
