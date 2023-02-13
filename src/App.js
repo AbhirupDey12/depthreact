f38ed63f87154a8df28a01d3e1772e4e1f87bc19
AbhirupDey12 <abhirupd470@gmail.com>
2023-02-13 17:34:59 +0530

    useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import React, { useState, useEffect, createContext } from "react";
-import './App.css';
-import Header from "./component/Header";
 // import Home from "./component/Home";
 // import About from "./component/About";
 // import Contact from "./component/Contact";
@@ -8,36 +5,45 @@ import Header from "./component/Header";
 // import Insta from "./component/Insta";
 // import Mail from "./component/Mail";
 // import UserDetails from "./component/UserDetails";
-import { BrowserRouter as Router, Switch, Route, Link, Routes } from 'react-router-dom';
-import CompA from "./component/CompA";
+// import { BrowserRouter as Router, Switch, Route, Link, Routes } from 'react-router-dom';
+// import CompA from "./component/CompA";
 
-const AppState = createContext();
-const NameContext = createContext();
+import React, { useReducer } from "react";
+import './App.css';
+import Header from "./component/Header";
+
+const reducer = ( state , action ) => {
+  
+  if(action.type === "INC") {
+    return state + 1 ;
+  }
+  else if(action.type === "DEC") {
+    return state - 1 ;
+  }
+  else if(action.type === "MUL") {
+    return state * 2 ;
+  }
+  else {
+    return state ;
+  }
+
+} ;
 
 export default function App() {
 
-  const [data, setdata] = useState('web3Mantra');
-  const [name, setName] = useState({ name: "Shubham", age: 20 });
-  const [temp, settemp] = useState(0) ;
-
-  const sayHello = () => {
-    alert("Hello User!");
-  }
+  const [ state , dispatch ] = useReducer(reducer , 0 ) ;
 
   return (
     <>
-      <AppState.Provider value={{ name, data, sayHello }}>
-        <NameContext.Provider value={temp}>
-          <Header />
-          <CompA />
-        </NameContext.Provider>
-      </AppState.Provider>
+        <Header />
+        <h1>{state}</h1>
+        <button onClick={ () => dispatch( { type : "INC" } ) } > Increment </button>
+        <button onClick={ () => dispatch( { type : "DEC" } ) } > Decrement </button>
+        <button  onClick={ () => dispatch( { type : "MUL" } ) }  > Multiplication </button>
     </>
   )
-
 };
 
-export { AppState , NameContext };
 
 
 
@@ -51,6 +57,67 @@ export { AppState , NameContext };
 
 
 
+
+
+
+
+
+
+
+
+
+// const [input, setInput] = useState("") ;
+//   const [count, setCount] = useState(0) ;
+
+//   const inputField = useRef() ;
+
+//   const formHandler = (e) => {
+//     setInput(e.target.value) ;
+//   }
+
+//   const clickHandler = () => {
+//     inputField.current.value = 'Hello world!' ;
+//     inputField.current.style.border = "2px solid gray" ;
+//     console.log(inputField.current) ;
+//   }
+
+//   return (
+//     <>
+
+//         <Header />
+
+//         <input ref={inputField} type="text" onChange={formHandler} value={input} />
+
+//         <br />
+
+//         <button onClick={clickHandler} > Clickk Here </button>
+
+//     </>
+//   )
+
+
+// export { AppState, NameContext };
+// const AppState = createContext();
+// const NameContext = createContext();
+
+// const [data, setdata] = useState('web3Mantra');
+// const [name, setName] = useState({ name: "Shubham", age: 20 });
+// const [temp, settemp] = useState(0) ;
+
+// const sayHello = () => {
+//   alert("Hello User!");
+// }
+
+// return (
+//   <>
+//     <AppState.Provider value={{ name, data, sayHello }}>
+//       <NameContext.Provider value={temp}>
+//         <Header />
+//         <CompA />
+//       </NameContext.Provider>
+//     </AppState.Provider>
+//   </>
+// )
 
 {/* <Router>
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -14,7 +14,7 @@ function Header() {
      return (
 
           <div className='header'>
-               React Context API          
+               <h1>  useRef Hook </h1>      
           </div>
 
      );
